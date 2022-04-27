26bb473d353710f1e4200b77bf9be71e8d11644c
Akshayan <thiruakshayan98@gmail.com>
2022-04-28 01:25:55 +0530

    Changes in Migration

---
+++ Rx.Infrastructure/Migrations/20220426184446_Changes.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rx.Infrastructure.Migrations
+{
+    public partial class Changes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Webhooks",
+                columns: table => new
+                {
+                    WebhookId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    SenderWebhookId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CustomerEmail = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CustomerName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProductPlanId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Webhooks", x => x.WebhookId);
+                    table.ForeignKey(
+                        name: "FK_Webhooks_ProductPlans_ProductPlanId",
+                        column: x => x.ProductPlanId,
+                        principalTable: "ProductPlans",
+                        principalColumn: "PlanId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Webhooks_ProductPlanId",
+                table: "Webhooks",
+                column: "ProductPlanId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Webhooks");
+        }
+    }
+}
