4fef081a3363fefe44f0d918f72635e3d8d6c98c
Akshayan <thiruakshayan98@gmail.com>
2022-06-16 14:42:48 +0530

    Migration

---
+++ Rx.Infrastructure/Migrations/PrimaryDb/20220616084440_PaymentDetails.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rx.Infrastructure.Migrations.PrimaryDb
+{
+    public partial class PaymentDetails : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentGatewayId",
+                table: "Organizations",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentMethodId",
+                table: "Organizations",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentGatewayId",
+                table: "Organizations");
+
+            migrationBuilder.DropColumn(
+                name: "PaymentMethodId",
+                table: "Organizations");
+        }
+    }
+}
