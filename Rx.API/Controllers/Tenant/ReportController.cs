190f0a3744bf4365273805f5b738c1a673bda0b0
Akshayan <thiruakshayan98@gmail.com>
2022-06-20 13:26:31 +0530

    Reports

--- Rx.API/Controllers/Tenant/ReportController.cs
+++ Rx.API/Controllers/Tenant/ReportController.cs
@@ -15,10 +15,54 @@ public class ReportController:ControllerBase
     {
         _mediator = mediator;
     }
-    [HttpGet]
+    [HttpGet("subscription-summary")]
     public async Task<IActionResult> GetSubscriptionStats()
     {
         var res = await _mediator.Send(new GetSubscriptionStatUseCase());
         return Ok(res);
     }
+    [HttpGet("subscription-activation")]
+    public async Task<IActionResult> GetSubscriptionActivation()
+    {
+        var res = await _mediator.Send(new GetSubscriptionActivationUseCase());
+        return Ok(res);
+    }
+    [HttpGet("sales-by-plan")]
+    public async Task<IActionResult> GetSalesByPlan()
+    {
+        var res = await _mediator.Send(new GetSalesByPlanUseCase());
+        return Ok(res);
+    }
+    [HttpGet("sales-by-addOn")]
+    public async Task<IActionResult> GetSalesByAddOn()
+    {
+        var res = await _mediator.Send(new GetSalesByAddonUseCase());
+        return Ok(res);
+    }
+    [HttpGet("upgrade")]
+    public async Task<IActionResult> GetUpgradeCount()
+    {
+        var res = await _mediator.Send(new GetUpgradeCountUseCase());
+        return Ok(res);
+    }
+    [HttpGet("downgrade")]
+    public async Task<IActionResult> GetDowngradeCount()
+    {
+        var res = await _mediator.Send(new GetDowngradeCountUseCase());
+        return Ok(res);
+    }
+    [HttpGet("unsubscribe")]
+    public async Task<IActionResult> GetUnsubscriptionCount()
+    {
+        var res = await _mediator.Send(new GetUnsubscriptionUseCase());
+        return Ok(res);
+    }
+    
+    [HttpGet("revenue")]
+    public async Task<IActionResult> GetRevenue()
+    {
+        var res = await _mediator.Send(new GetRevenueStatsUseCase());
+        return Ok(res);
+    }
+    
 }
---
+++ Rx.Application/UseCases/Tenant/Report/GetSalesByPlanUseCase.cs
@@ -0,0 +1,38 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Rx.Domain.DTOs.Tenant.Report;
+using Rx.Domain.Interfaces.DbContext;
+
+namespace Rx.Application.UseCases.Tenant.Report;
+
+public record GetSalesByPlanUseCase():IRequest<IEnumerable<Stats>>;
+
+public class GetSalesByPlanUseCaseHandler : IRequestHandler<GetSalesByPlanUseCase, IEnumerable<Stats>>
+{
+    private readonly ITenantDbContext _tenantDbContext;
+
+    public GetSalesByPlanUseCaseHandler(ITenantDbContext tenantDbContext)
+    {
+        _tenantDbContext = tenantDbContext;
+    }
+    public async Task<IEnumerable<Stats>> Handle(GetSalesByPlanUseCase request, CancellationToken cancellationToken)
+    {
+        var result =await _tenantDbContext.Subscriptions!
+            .Include(s=>s.ProductPlan)
+            .Include(s=>s.ProductPlan.Product)
+            .Select(x=>
+                new
+                {
+                    pName = x.ProductPlan.Name,x.ProductPlanId,ppName=x.ProductPlan.Product.Name,x.ProductPlan
+                }
+            )
+            .ToListAsync(cancellationToken: cancellationToken);
+        var statCount = result.GroupBy(s => s.ProductPlan).Select(x => new Stats(
+                x.Key.Product.Name + "/" + x.Key.Name,
+                x.Count()
+            )
+        ).ToList();
+       
+        return statCount;
+    }
+}
